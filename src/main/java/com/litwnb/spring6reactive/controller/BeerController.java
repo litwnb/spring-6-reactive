ca0cada86f3276ca1fe88c7157f8375906c3d6af
Kostiantyn Mamaisur <kmamaisur@gmail.com>
2025-05-27 21:48:45 +0300

    added put method

--- src/main/java/com/litwnb/spring6reactive/controller/BeerController.java
+++ src/main/java/com/litwnb/spring6reactive/controller/BeerController.java
@@ -17,6 +17,13 @@ public class BeerController {
 
     private final BeerService beerService;
 
+    @PutMapping(BEER_PATH_ID)
+    Mono<ResponseEntity<Void>> updateExistingBeer(@PathVariable("beerId") Integer beerId,
+                                                  @RequestBody BeerDTO beerDTO) {
+        return beerService.updateBeer(beerId, beerDTO)
+                .map(savedBeer -> ResponseEntity.ok().build());
+    }
+
     @PostMapping(BEER_PATH)
     Mono<ResponseEntity<Void>> createNewBeer(@RequestBody BeerDTO beerDTO) {
         return beerService.saveNewBeer(beerDTO)
--- src/main/java/com/litwnb/spring6reactive/service/BeerService.java
+++ src/main/java/com/litwnb/spring6reactive/service/BeerService.java
@@ -1,6 +1,7 @@
 package com.litwnb.spring6reactive.service;
 
 import com.litwnb.spring6reactive.model.BeerDTO;
+import org.springframework.http.ResponseEntity;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -10,4 +11,6 @@ public interface BeerService {
     Mono<BeerDTO> getBeerById(Integer beerId);
 
     Mono<BeerDTO> saveNewBeer(BeerDTO beerDTO);
+
+    Mono<BeerDTO> updateBeer(Integer beerId, BeerDTO beerDTO);
 }
