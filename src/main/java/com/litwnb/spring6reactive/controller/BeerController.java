5ba4f3fd5b2ca5d836d893b2d4c4b48c531abbd6
Kostiantyn Mamaisur <kmamaisur@gmail.com>
2025-05-27 20:08:37 +0300

    added post method

--- src/main/java/com/litwnb/spring6reactive/controller/BeerController.java
+++ src/main/java/com/litwnb/spring6reactive/controller/BeerController.java
@@ -3,9 +3,12 @@ package com.litwnb.spring6reactive.controller;
 import com.litwnb.spring6reactive.model.BeerDTO;
 import com.litwnb.spring6reactive.service.BeerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -16,6 +19,15 @@ public class BeerController {
     public static final String BEER_PATH_ID = BEER_PATH + "/{beerId}";
 
     private final BeerService beerService;
+
+    @PostMapping(BEER_PATH)
+    Mono<ResponseEntity<Void>> createNewBeer(BeerDTO beerDTO) {
+        return beerService.saveNewBeer(beerDTO)
+                .map(savedDto -> ResponseEntity.created(UriComponentsBuilder
+                        .fromPath("http://localhost:8080/" + BEER_PATH + "/" + savedDto.getId())
+                        .build().toUri())
+                        .build());
+    }
 
     @GetMapping(BEER_PATH_ID)
     Mono<BeerDTO> getBeerById(@PathVariable("beerId") Integer beerId) {
