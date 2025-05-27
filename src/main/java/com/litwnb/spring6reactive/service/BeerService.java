e03a4d7fb4e86b0d722d9cc4ca734eb5d2f66b77
Kostiantyn Mamaisur <kmamaisur@gmail.com>
2025-05-27 19:31:51 +0300

    added path variables

--- src/main/java/com/litwnb/spring6reactive/service/BeerService.java
+++ src/main/java/com/litwnb/spring6reactive/service/BeerService.java
@@ -2,7 +2,10 @@ package com.litwnb.spring6reactive.service;
 
 import com.litwnb.spring6reactive.model.BeerDTO;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface BeerService {
     Flux<BeerDTO> listBeers();
+
+    Mono<BeerDTO> getBeerById(Integer beerId);
 }
